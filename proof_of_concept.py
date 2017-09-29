6f96f4de70e80442f5260a46c9b6fdd0e3893d6d
james <hhsdstaff@gmail.com>
2017-09-29 12:38:14 -0700

    Initial test of Docx; reading out specific field from MDB and generating .DOCX files.

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,7 +1,7 @@
 import pyodbc
+from docx import Document
 
-
-print [x for x in pyodbc.drivers() if x.startswith('Microsoft Access Driver')]
+# print [x for x in pyodbc.drivers() if x.startswith('Microsoft Access Driver')]
 
 conn_str = (
     r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
@@ -12,4 +12,10 @@ connection = pyodbc.connect(conn_str)
 cursor = connection.cursor()
 
 for item in cursor.execute("SELECT TOP 10 * FROM tblRSVPCases"):
-    print item
+    output_doc = Document()
+
+    case_name = str(item[5]).replace(r'/', '-')
+
+    output_doc.save(r"output/" + case_name + '.docx')
+
+
