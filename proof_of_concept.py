9ad4035016023ee14b99da22856bb776d98b32df
james <hhsdstaff@gmail.com>
2017-10-06 14:02:26 -0700

    WIP: adopting the other parts which were modularized

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,22 +1,49 @@
 import pyodbc
 from docx import Document
+from docx.enum.section import WD_ORIENTATION
+
+from phonelog import PhoneLog
+from queries.qryTestGroupPeopleByCase import QUERY_CASES_AND_PEOPLE
 
 # print [x for x in pyodbc.drivers() if x.startswith('Microsoft Access Driver')]
 
-conn_str = (
+CONN_STR = (
     r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
     r'DBQ=C:\RSVP_Export_Process\RSVP_Backup_20170928.mdb;'
     )
 
-connection = pyodbc.connect(conn_str)
-cursor = connection.cursor()
 
-for item in cursor.execute("SELECT TOP 10 * FROM tblRSVPCases"):
-    output_doc = Document()
-    case_name = str(item[5]).replace(r'/', '-')
-    case_id = str(item[6])
+def get_cases_from_rsvp_mdb():
+    connection = pyodbc.connect(CONN_STR)
+    cursor = connection.cursor()
+    results = {}
 
-    output_doc.add_paragraph(case_id)
-    output_doc.save(r"output/" + case_name + '.docx')
+    for item in cursor.execute(QUERY_CASES_AND_PEOPLE):
+
+        case_id = item[0]
+
+        if not results.has_key(case_id):
+            results[case_id] = []
+        results[case_id].append(item)
+
+    return results
 
 
+def format_output_filename(case_name):
+    return case_name.strip().replace(r'/', '-')
+
+
+cases = get_cases_from_rsvp_mdb()
+for case_id in cases.keys():
+
+    if len(cases[case_id]) == 0:
+        print(str.format("case id {0} has no associated people", case_id))
+
+    case_name = str(cases[case_id][0][1])
+    if not case_name.isspace():
+        case_name = format_output_filename(case_name)
+
+    people = cases[case_id]
+    phone_log = PhoneLog(case_name)
+    phone_log.add_people(people)
+    phone_log.save()
